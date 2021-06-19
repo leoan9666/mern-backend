1dd5371428349e3c35f14bfb1cc016a18f4a96d9
leoan9666 <85979876+leoan9666@users.noreply.github.com>
2021-06-20 02:14:05 +0800

    complete backend api features (#10) (#11)

--- util/location.js
+++ util/location.js
@@ -6,9 +6,8 @@ const getCoordsForAddress = async (address) => {
   const coordinates = await createLatLngPromise(address);
 
   if (!coordinates || coordinates.status === "ZERO_RESULTS") {
-    throw new HttpError(
-      "Could not find location for the specified address.",
-      422
+    return next(
+      new HttpError("Could not find location for the specified address.", 422)
     );
   }
   return coordinates;
@@ -16,7 +15,7 @@ const getCoordsForAddress = async (address) => {
 
 const createLatLngPromise = async (address) => {
   return new Promise((resolve, reject) => {
-    resolve({ lat: 40.7484474, log: -73.9871516 });
+    resolve({ lat: 40.7484474, lng: -73.9871516 });
   });
 };
 
