be776d714b080c3a8d1cb6e55b8aabbb7e4b6985
leoan9666 <85979876+leoan9666@users.noreply.github.com>
2021-06-19 16:48:07 +0800

    complete place routes and its controller (#5) (#6)

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const placeRoutes = require("./routes/place-routes");
 
+const HttpError = require("./models/http-model");
+
 const app = express();
+
+app.use(express.json());
+
 app.use("/api/places", placeRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route.", 404);
+});
 
 app.use((error, req, res, next) => {
   if (res.headersSent) {
--- routes/place-routes.js
+++ routes/place-routes.js
@@ -1,48 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const HttpError = require("../models/http-model");
+const placesController = require("../controllers/places-controller");
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous skyscrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      log: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001, United States",
-    creater: "u1",
-  },
-];
+router.get("/:pid", placesController.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const places = DUMMY_PLACES.find((place) => {
-    return place.id === placeId;
-  });
+router.get("/user/:uid", placesController.getPlacesByUserId);
 
-  if (!places) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
+router.post("/", placesController.createPlace);
 
-  res.status(200).json({ place });
-});
+router.patch("/:pid", placesController.updatePlace);
 
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const places = DUMMY_PLACES.find((place) => {
-    return place.creater === userId;
-  });
-
-  if (!places) {
-    return next(
-      new HttpError("Could not find a place for the provided id.", 404)
-    );
-  }
-
-  res.status(200).json({ places });
-});
+router.delete("/:pid", placesController.deletePlace);
 
 module.exports = router;
